002f3939d8d1478b46905ea110ee8805302ed5cd
Milton Josué Urbina <umiltonjosue@gmail.com>
2022-06-16 18:35:29 -0500

    borrar calculadora

--- Calculadora_comisiones.py
+++
@@ -1,6 +0,0 @@
-nombre = input("Cual es tu nombre? ")
-ventas = round(float((input("Cuanto has vendido este mes? "))), 2)
-
-comisiones = round ( ventas * 13 / 100 , 2 )
-
-print(f"Hola {nombre} tus ventas fueron ${ventas} por lo que tu comision es de ${comisiones}")
