31e8dd0a8162451fdd09aff541b4790b0bdb3e40
Milton Josué Urbina <umiltonjosue@gmail.com>
2022-06-21 17:01:20 -0500

    Crear listas en python

---
+++ Listas.py
@@ -0,0 +1,17 @@
+#mi_lista = ['a', 'b', 'c']
+#resultado = len(mi_lista)
+#resultado = mi_lista[0:]
+#mi_lista2 = ['d', 'e', 'f']
+#mi_lista3 = mi_lista + mi_lista2
+
+#mi_lista3[0] = 'alfa'
+#mi_lista3.append('g') #agrega elementos
+
+#eliminando = mi_lista3.pop(0) #elimina un elemnto, si dejamos en blanco elimina el ultimo elemento
+#print(mi_lista3)
+#print(eliminando)
+
+lista = ['g' , 'o', 'b', 'm', 'c']
+lista.sort() #no es correcto colocarlo en una variable
+lista.reverse()#pone la lista en orden al revez
+print(lista)
