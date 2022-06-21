3b02e8143a200e2c8ed6d240546454438b16bd74
Milton Josué Urbina <umiltonjosue@gmail.com>
2022-06-21 02:03:38 -0500

    Propiedades para los strings Python

---
+++ Propiedades.py
@@ -0,0 +1,14 @@
+#nombre = "Carina"
+#nombre[0] = "K"         #los strings no pueden ser cambiados, son inmutables
+#print(nombre)
+
+#n1= "Kari"
+#n2= "na"                #Los strings se pueden multiplicar
+#print(n1 * 5)
+
+poema = """Mil pequenios peces blancos
+como si hirivera                            
+el color del agua."""    #metodo para agregar saltos de linea en un string
+
+#print("agua" in poema) #dice si la palabra se encuentra la palabra agua en la variable poema con un resultado true\false
+print("agua" not in poema) #negacion si existe la palabra agua en la variable poema
