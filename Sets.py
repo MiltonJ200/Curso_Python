20a07cd303875407c6d3d57909ad23e5c5beb699
Milton Josué Urbina <umiltonjosue@gmail.com>
2022-06-24 00:09:57 -0500

    update sets

--- Sets.py
+++ Sets.py
@@ -1,6 +1,6 @@
-mi_set = set([1,2,3,4,(1,2,3),5]) #no se pueden repetir los valores ya existentes y se pueden agregat tuples
-print(type(mi_set))
-print(mi_set)
+#mi_set = set((1,2,3,4,5)) #no se pueden repetir los valores ya existentes y se pueden agregat tuples
+
+#print(2 in mi_set) comprobar si el 2 esta en el set
 
 #otro_set = {1, 2, 3}
 #print(type(otro_set))
@@ -8,3 +8,13 @@ print(mi_set)
 
 #mi_set[0]  los sets no son modificables.
 
+s1 = {1,2,3}
+#s2 = {3,4,5}
+#s3 = s1.union(s2) #une sets
+#s1.add(4) #agrega el valor 4 en el set
+#s1.remove(3)#remueve un elemento en el set
+#s1.discard(6)#sirve para descartar elementos
+#s1.pop()#elimina un elemento aleatorio
+#sorteo = s1.pop() #ejemplo de uso de pop
+#s1.clear()#limpia todo el set
+print(s1)
