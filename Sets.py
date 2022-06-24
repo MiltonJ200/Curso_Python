0c50efdac1d313206257dfc19c20c5802eed08fd
Milton Josué Urbina <umiltonjosue@gmail.com>
2022-06-23 23:58:53 -0500

    update

--- Sets.py
+++ Sets.py
@@ -1,3 +1,10 @@
-mi_set = set([1,2,3,4,5])
+mi_set = set([1,2,3,4,4,4,4,5,5,5,5]) #no se pueden repetir los valores ya existentes
 print(type(mi_set))
 print(mi_set)
+
+#otro_set = {1, 2, 3}
+#print(type(otro_set))
+#print(otro_set)
+
+#mi_set[0]  los sets no son modificables.
+
