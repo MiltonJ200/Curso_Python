e94a164ac66aedd04ec70c730fd912f6c58e6f8f
Milton Josué Urbina <umiltonjosue@gmail.com>
2022-06-24 01:00:59 -0500

    update booleans

--- Booleanos.py
+++ Booleanos.py
@@ -4,6 +4,10 @@
 #print(type(var1))
 #print(var1)
 
-numero = 5 == 2+3
-print(type(numero))
-print(numero)
+#numero = 5 != 2+3
+#numero = bool(5<6) #otra forma de usar un boolean
+list = [1,2,3,4]
+control = 5 in list
+print(type(control))
+print(control)
+
