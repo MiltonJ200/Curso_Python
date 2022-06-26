ef9349e3dd8d5f2f4aa90251b0aae0e48f881cb2
Milton Josué Urbina <umiltonjosue@gmail.com>
2022-06-25 20:52:11 -0500

    Update de ejercicios booleanos

--- Booleanos.py
+++ Booleanos.py
@@ -11,3 +11,18 @@ control = 5 in list
 print(type(control))
 print(control)
 
+#Ejercicios
+
+#Ejercicio 1
+prueba = 2<1
+
+#Ejercicio 2
+v1 = 17834/34
+v2 = 87*56
+prueba = v1 > v2
+print(prueba)
+
+#Ejercicio 3
+raiz = 25 ** 0.5
+prueba = raiz == 5
+print(prueba)
