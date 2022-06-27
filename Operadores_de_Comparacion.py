92fa8e43ba2683c2fc591701fbbd5e2f0db33f86
Milton Josué Urbina <umiltonjosue@gmail.com>
2022-06-26 23:17:23 -0500

    Create Operadores_de_Comparacion.py

---
+++ Operadores_de_Comparacion.py
@@ -0,0 +1,32 @@
+#mi_bool = 10==25
+#print(mi_bool)
+
+#mi_bool = 10==10
+#print(mi_bool)
+
+#mi_bool = 5+5==18-8
+#print(mi_bool)
+
+mi_bool = "blanco" == "negro"
+print(mi_bool)
+
+mi_bool = "blanco"=="blanco"
+print(mi_bool)
+
+mi_bool = "blanco"=="Blanco".lower()
+print(mi_bool)
+
+mi_bool = "100" == 100
+print(mi_bool)
+
+mi_bool = 100.0 == 100
+print(mi_bool)
+
+mi_bool = 100 != 99
+print(mi_bool)
+
+mi_bool = 100 != 100
+print(mi_bool)
+
+mi_bool = 5 >= 6
+print(mi_bool)
